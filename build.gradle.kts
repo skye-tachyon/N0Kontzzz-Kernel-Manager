75bbe6c0d7205cdfa04c49f10544b77769d6740c
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2025-12-23 09:32:33 +0700

    chore: Bump Kotlin version to 2.3.0
    This commit updates the Kotlin version used in the project from 2.2.21 to 2.3.0. This includes updating the `org.jetbrains.kotlin.android` and `org.jetbrains.kotlin.plugin.serialization` plugins.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.android") version "2.3.0" apply false
     id("com.google.dagger.hilt.android") version "2.57.2" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.2.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.ksp) apply false
 }
