a5a51af546e202e8c1101be1da67443fd77cf7f8
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2026-01-12 12:50:27 +0700

    NKM: release 1.5.4-hotfix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "id.nkz.nokontzzzmanager"
         minSdk = 26
         targetSdk = 36
-        versionCode = 66
-        versionName = "1.5.4"
+        versionCode = 67
+        versionName = "1.5.4-hotfix"
     }
     buildTypes {
         release {
