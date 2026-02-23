5987f03e38cf914206ba1fd66b59d79a37ebb501
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2026-02-23 08:28:18 +0700

    refactor: Remove unused FreqConstants and ProcConstants files
    This commit deletes the `FreqConstants.kt` and `ProcConstants.kt` files as they are no longer used in the project. These files contained constant values for sysfs and procfs paths related to CPU/GPU frequencies and TCP congestion algorithms.

--- app/src/main/java/id/nkz/nokontzzzmanager/utils/ProcConstants.kt
+++
@@ -1,9 +0,0 @@
-package id.xms.xtrakernelmanager.utils
-
-object ProcConstants {
-    // TCP congestion control paths
-    const val TCP_CONGESTION_ALGORITHM = "/proc/sys/net/ipv4/tcp_congestion_control"
-    const val TCP_AVAILABLE_CONGESTION_ALGORITHM = "/proc/sys/net/ipv4/tcp_available_congestion_control"
-    
-    // Other proc filesystem paths can be added here
-}
