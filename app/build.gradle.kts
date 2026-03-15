a0e7d97ec8493b9a623ec0769c281831e74b5d92
bimoalfarrabi <bimoalfarrabi24@gmail.com>
2026-03-15 11:35:22 +0700

    NKM: release 1.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ configure <com.android.build.api.dsl.ApplicationExtension> {
         applicationId = "id.nkz.nokontzzzmanager"
         minSdk = 31
         targetSdk = 36
-        versionCode = 100
-        versionName = "1.6.5"
+        versionCode = 106
+        versionName = "1.7.0"
     }
     buildTypes {
         release {
